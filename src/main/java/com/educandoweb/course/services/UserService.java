6039da0b594a0501c75ae6dd5f734bcbe8e48442
Jacob <crossfire_brasil2013@hotmail.com>
2023-11-08 00:21:01 -0300

    Exception delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -30,7 +30,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(RuntimeException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
